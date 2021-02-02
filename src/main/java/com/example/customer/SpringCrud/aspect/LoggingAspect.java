3db6367a2359ab84822c9cc753d9c2a14647fe25
ethany21 <imwoodam@hanmail.net>
2021-02-03 00:19:09 +0900

    added around aspect pointcut to methods in CustomerServiceImpl

--- src/main/java/com/example/customer/SpringCrud/aspect/LoggingAspect.java
+++ src/main/java/com/example/customer/SpringCrud/aspect/LoggingAspect.java
@@ -2,19 +2,35 @@ package com.example.customer.SpringCrud.aspect;
 
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Around("execution(* com.example.customer.SpringCrud.service.CustomerServiceImpl.*(..))")
+    public Object aroundServices(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+
+        long begin = System.currentTimeMillis();
+        logger.info("This transaction is on service of "+ proceedingJoinPoint.getSignature().toShortString());
+
+        Object result = proceedingJoinPoint.proceed();
+
+        long end = System.currentTimeMillis();
+
+        long time = end  - begin;
+        logger.info("This transaction took "+ time);
+        return result;
+    }
 
     @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
     public void GetMapping(){}
